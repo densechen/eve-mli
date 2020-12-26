caf6f589ae6633ea017dbd8ce62837d3145ae9ad
densechen <densechen@foxmail.com>
2020-12-27 00:09:55 +0800

    fix doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../eve'))
+sys.path.insert(0, os.path.abspath('../eve'))
 
 # -- Project information -----------------------------------------------------
 
