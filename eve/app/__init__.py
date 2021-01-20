8648d4efd764400ae2d5aae8078f0b0651c4d557
densechen <densechen@foxmail.com>
2021-01-20 07:32:41 +0000

    update

--- eve/app/__init__.py
+++ eve/app/__init__.py
@@ -1,6 +1,6 @@
 from .algorithm import ALGOS
 from .exp_manager import ExperimentManager
-from .model import BaseModel
+from .model import BaseModel, Classifier
 from .space import EveSpace
 from .trainer import BaseTrainer
 from .upgrader import Upgrader
